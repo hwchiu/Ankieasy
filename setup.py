069296aecef1037bf61976db92ab5df1aeda7b56
hwchiu <hwchiu@linkernetworks.com>
2017-09-12 22:22:57 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+requirements = [
+	'bs4',
+	'lxml',
+	'decorator',
+	'pyaudio',
+	'wget',
+	]
+
+setup(
+	name = "anki_auto_add",
+	version = "1.0",
+	author = "Hung-Wei Chiu/Yu-Hsien Yeh",
+	author_email = "sppsorrg@gmail.com/ex8600@gmail.com",
+	description = ("An tool of how to automatically lookup words and insert into Anki decks"),
+	install_requires=requirements
+)
+
