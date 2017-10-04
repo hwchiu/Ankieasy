39d46512af3ceef096cffef74cce55e6fcae7bef
YuHsien <ex8600@gmail.com>
2017-10-04 17:08:02 +0800

    Finish the Cambridge Dict crawler

--- module/english_cambridge.py
+++ module/english_cambridge.py
@@ -6,6 +6,7 @@ import platform
 import datetime
 import json
 import wget
+import re
 
 def LookUp(word, data):
     # Eliminate the end of line delimiter
@@ -16,12 +17,25 @@ def LookUp(word, data):
     content = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(content, 'lxml')
     result = {}
-    front_word = word + '\n'
-    back_word = ""
-    download_dir = ""
+    front_word = word + '<br>'
+    back_word = ''
+    download_dir = ''
+    guideWordStyleHead = '<font color="yellow"><b>'
+    guideWordStyleTail = '</b></font>'
+    posStyleHead = '<font color=#00fff9>'
+    posStyleTail = '</font>'
     cnt = 1
 
+    if "download_dir" in data:
+        download_dir = data['download_dir']
+
+    if word == "":
+        return False
+
     tabEntry = soup.find('div', class_='tabs tabs-entry js-tabs-wrap js-toc')
+    if(tabEntry is None):
+        return False
+
     english = tabEntry.find('div', class_='tabs__content on', attrs={"data-tab": "ds-american-english"})
     if(english is None):
         english = tabEntry.find('div', class_='tabs__content on', attrs={"data-tab": "ds-british"})
@@ -29,18 +43,22 @@ def LookUp(word, data):
             english = tabEntry.find('div', class_='tabs__content on', attrs={"data-tab": "ds-business-english"})
 
     partOfSpeech = english.find_all('div', class_='entry-body__el clrd js-share-holder')
+    sound = partOfSpeech[0].find('span', attrs={'data-src-mp3':True})['data-src-mp3']
+    if(sound is not None):
+        wget.download(sound, out=download_dir+"Py_"+word+".mp3")
+        front_word = "[sound:Py_"+word+".mp3]" + front_word
     for i in range(0,len(partOfSpeech)):
         posgram = partOfSpeech[i].find('span', class_='posgram ico-bg')
         if(posgram is not None):
             pos = posgram.find('span').get_text() # get POS
-            front_word += pos + '\n'
-            back_word += pos + '\n'
+            front_word += posStyleHead + '(' + pos + ')' + posStyleTail + '<br>'
+            back_word +=  posStyleHead + '(' + pos + ')' + posStyleTail + '<br>'
         senseBlock = partOfSpeech[i].find_all('div', class_='sense-block')
         for j in range(0,len(senseBlock)):
             guideWord = senseBlock[j].find('span', class_='guideword') # get the guide word ex.(BREAK)
             if(guideWord is not None):
                 guideWordClear = guideWord.find('span').get_text()
-                back_word += '\t' + '(' + guideWordClear + ')' + '\n'
+                back_word += guideWordStyleHead + '(' + guideWordClear + ')' + guideWordStyleTail + '<br>'
             defBlock = senseBlock[j].find_all('div', class_='def-block pad-indent')
             for k in range(0,len(defBlock)):
                 # English explain
@@ -48,15 +66,15 @@ def LookUp(word, data):
                 if(explain[-2] == ':'):
                     tmp = explain[:-2]
                     explain = tmp + '.' # Replace the colon to dot
-                back_word += '\t\t' + str(cnt) + '. ' + explain + '\n'
+                back_word += str(cnt) + '. ' + explain + '<br>'
 
                 # example sentence
                 defBody = defBlock[k].find('span', class_='def-body')
                 if(defBody is not None):
                     exampleSentence = defBody.find('span', class_='eg').get_text() # get example sentence
-                    front_word += str(cnt) + '. ' + exampleSentence + '\n'
+                    front_word += str(cnt) + '. ' + exampleSentence + '<br>'
                 else:
-                    front_word += str(cnt) + '. ' + '\n'
+                    front_word += str(cnt) + '. ' + '<br>'
                 cnt += 1
 
     # Some meaning will reveal the "word" in back_word
@@ -64,8 +82,8 @@ def LookUp(word, data):
 
     result['front_word'] = front_word
     result['back_word'] = back_word
-    print('<<<'+word+'>>>'+'\n')
-    print('front_word = '+'\n'+front_word)
-    print('back_word = '+'\n'+back_word)
-    print('-----------------------------------------')
+    # print('<<<'+word+'>>>'+'\n')
+    # print('front_word = '+'\n'+front_word)
+    # print('back_word = '+'\n'+back_word)
+    # print('-----------------------------------------')
     return result
