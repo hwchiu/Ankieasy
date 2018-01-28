998b758f9fae5c40e63955ac4430f781fca4e148
YuHsien <ex8600@gmail.com>
2018-01-28 11:29:39 +0800

    Complete the jp_verb.py

--- module/japanese_verb.py
+++ module/japanese_verb.py
@@ -37,8 +37,7 @@ def getVerb(tr, typeArray, front_word, download_dir):
     result['front_word'] = front_word
     return result
 
-def getJishoMasu(tr, typeArray, front_word, download_dir):
-    result = {}
+def getJishoMasu(tr, front_word, download_dir):
     back_word = ''
     midashi = tr.find('td', class_='midashi')
     midashiWrapper = midashi.find('div', class_='midashi_wrapper')
@@ -46,7 +45,7 @@ def getJishoMasu(tr, typeArray, front_word, download_dir):
     jisho_masu = jisho_masu.split(chr(10))[1]
 
     jisho_masuCnt = 0
-    for typeStr in typeArray:
+    for typeStr in ['jisho', 'masu']:
         typeTd = tr.find('td', class_='katsuyo katsuyo_' + typeStr + '_js')
         soundDiv = typeTd.find('div', class_='katsuyo_proc_button clearfix')
         if soundDiv != None:
@@ -61,8 +60,7 @@ def getJishoMasu(tr, typeArray, front_word, download_dir):
                 print('OJAD_err=', err)
             front_word += jisho_masu.split('・')[jisho_masuCnt] + '<br>'
             jisho_masuCnt += 1
-    result['front_word'] = front_word
-    return result
+    return front_word
 
 def LookUp(word, data, download_dir):
     
@@ -105,11 +103,67 @@ def LookUp(word, data, download_dir):
 
     tbody = table.find('tbody')
     tbodyTr = tbody.find('tr')
-    # result = getVerb(tbodyTr, ['jisho', 'masu', 'te', 'ta', 'nai', 'nakatta', 'ba', 'shieki', 'ukemi', 'meirei', 'kano', 'ishi'], front_word, download_dir)
-    # result = getVerb(tbodyTr, ['jisho', 'masu'], front_word, download_dir)
-    result = getJishoMasu(tbodyTr, ['jisho', 'masu'], front_word, download_dir)
-    result['back_word'] = back_word
-    result['read_word'] = read_word
-    print(result)
-    return result
+    # front_word = getVerb(tbodyTr, ['jisho', 'masu', 'te', 'ta', 'nai', 'nakatta', 'ba', 'shieki', 'ukemi', 'meirei', 'kano', 'ishi'], front_word, download_dir)
+    # front_word = getVerb(tbodyTr, ['jisho', 'masu'], front_word, download_dir)
+    front_word = getJishoMasu(tbodyTr, front_word, download_dir)
+
+    body = hj_Soup.find('body', attrs={'onload': 'onInit();', 'onmousedown': 'MouseDownOnBody(event);'})
+    if body != None:
+        wrapper = body.find('div', id='wrapper')
+        webboxContent = wrapper.find('div', id='webbox-content')
+        mainBlock = webboxContent.find('div', id='main')
+        mainContainer = mainBlock.find('div', class_='mian_container main_container')
+        
+        headwordJpCnt = 1
+        headwordJpStr = 'headword_jp_' + str(headwordJpCnt)
+
+        while mainContainer.find('div', id=headwordJpStr, class_='jp_word_comment') != None:
+            wordBlock = mainContainer.find('div', id=headwordJpStr, class_='jp_word_comment')
+            wordExt = wordBlock.find('div', class_='word_ext_con clearfix')
+            partOfSpeech = wordExt.find_all('div', class_='flag big_type tip_content_item')
+            posMeaningBlock = wordExt.find_all('ul', class_='tip_content_item jp_definition_com')
+            for i in range(0, len(posMeaningBlock)):
+                if len(partOfSpeech) >= i+1:
+                    back_word  += '(' + HanziConv.toTraditional(partOfSpeech[i]['title']) + ')' + '<br>'
+                posMeaning = posMeaningBlock[i].find_all('li', class_='flag')
+                meaningCnt = 1
+                for j in range(0, len(posMeaning)):
+                    meaning = posMeaning[j].find('span', class_='word_comment soundmark_color')
+                    if meaning == None:
+                        meaning = posMeaning[j].find('span', class_='jp_explain soundmark_color')
+                    meaningText = meaning.get_text()
+                    if meaningText.find('（') != -1:
+                        meaningText = meaningText[0:meaningText.find('（')]  # Truncate the content after '（'
+                    meaningText = meaningText.replace('。', '')              # Remove the '。'
+                    if len(posMeaning) != 1:  
+                        back_word += str(meaningCnt) + '. ' # When there is only one meaning, remove the '1.'
+                    back_word += meaningText + '<br>'
+
+                    exSentStr = ''
+                    exSentCnt = 0
+                    exSentNum = 1 # How many example sentences you want in the card
+                    exSentence = posMeaning[j].contents[1] # The second <div> in <li> in <ul class='tip_content_item jp_definition_com'>
+                    for child in exSentence.children:
+                        try:
+                            exSentStr += child.text
+                            if child.name == 'br': # Only take the first example sentence
+                                front_word += str(meaningCnt) + '. ' + exSentStr.split('/')[0] + '<br>'
+                                back_word += exSentStr.split('/')[1] + '<br>'
+                                exSentStr = ''
+                                exSentCnt += 1
+                                if exSentCnt == exSentNum:
+                                    break
+                        except:
+                            exSentStr += child
+                    meaningCnt += 1
+            headwordJpCnt += 1
+            headwordJpStr = 'headword_jp_' + str(headwordJpCnt)
+        result['front_word'] = front_word
+        result['back_word'] = HanziConv.toTraditional(back_word)
+        result['read_word'] = read_word
+        print(result)
+        return result
+    else:
+        print('Get information failed!')
+        return False
 
